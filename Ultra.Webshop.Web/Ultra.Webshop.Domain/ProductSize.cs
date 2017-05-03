d9d56d51a88d2809da17ffad8e0bf2180c4a49a3
Gilles Coenraets <gilles.coenraets@ultramg.com>
2017-05-03 17:19:26 +0200

    slight modif

--- Ultra.Webshop.Web/Ultra.Webshop.Domain/ProductSize.cs
+++ Ultra.Webshop.Web/Ultra.Webshop.Domain/ProductSize.cs
@@ -4,19 +4,10 @@ namespace Ultra.Webshop.Domain
 {
     public class ProductSize
     {
-        public SizeType SizeType { get; set; }
-
         public string Name { get; set; }
 
         public string Description { get; set; }
 
         public string Value { get; set; }
-    }
-
-    public enum SizeType
-    {
-        None,
-        Garment,
-        Shoe
     }
 }
